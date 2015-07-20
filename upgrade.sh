c00f5ed9812c0bd9f4001db739057e64edcfc32d
Vladimir Kozhukalov <vkozhukalov@mirantis.com>
2015-07-20 20:49:55 +0300

    Add some infrastructure and executable files
    * .gitignore
    * .gitreview
    * run_tests.sh
    * specs/fuel-upgrade.spec
    * upgrade.sh
    
    Change-Id: Iebe8905194c353e8194776c4950a18ddd560082a

---
+++ upgrade.sh
@@ -0,0 +1,98 @@
+#!/bin/bash
+#    Copyright 2015 Mirantis, Inc.
+#
+#    Licensed under the Apache License, Version 2.0 (the "License"); you may
+#    not use this file except in compliance with the License. You may obtain
+#    a copy of the License at
+#
+#         http://www.apache.org/licenses/LICENSE-2.0
+#
+#    Unless required by applicable law or agreed to in writing, software
+#    distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+#    WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+#    License for the specific language governing permissions and limitations
+#    under the License.
+
+SCRIPT_PATH=$(dirname $(readlink -e $0))
+UPGRADE_PATH=$SCRIPT_PATH/upgrade
+VIRTUALENV_PATH=$UPGRADE_PATH/.fuel-upgrade-venv
+UPGRADERS=${UPGRADERS:-host-system docker openstack}
+LOCK_FILE=/var/lock/fuel_upgarde.lock
+
+
+function error {
+  local message="$1"
+  local code="${2:-1}"
+
+  echo "${message}"
+
+  exit "${code}"
+}
+
+
+function prepare_virtualenv {
+
+  if ! which virtualenv >/dev/null; then
+    yum -y install python-virtualenv || error "Failed to install python-virtualenv"
+  fi
+
+  rm -rf $VIRTUALENV_PATH
+  virtualenv $VIRTUALENV_PATH
+  $VIRTUALENV_PATH/bin/pip install fuel_upgrade --no-index --find-links file://$UPGRADE_PATH/deps || error "Failed to install fuel_upgrade script"
+}
+
+
+function run_upgrade {
+  # prepare virtualenv for fuel_upgrade script
+  prepare_virtualenv
+
+  # run fuel_upgrade script
+  $VIRTUALENV_PATH/bin/python "$VIRTUALENV_PATH/bin/fuel-upgrade" --src "$UPGRADE_PATH" $UPGRADERS "$@" || error "Upgrade failed" $?
+}
+
+
+function switch_to_version {
+  version=$1
+  version_path=/etc/fuel/$version/version.yaml
+
+  if [ ! -f $version_path ]; then
+    error "Version ${version} not found"
+  fi
+
+  # Replace symlink to current version
+  ln -sf $version_path /etc/fuel/version.yaml
+  # Replace symlink to supervisor scripts
+  ln -nsf /etc/supervisord.d/$version /etc/supervisord.d/current
+  # Stop all supervisor services
+  supervisorctl stop all &
+  # And at the same time stop all docker containers
+  docker stop -t=4 $(docker ps -q)
+  # Restart supervisor
+  service supervisord restart
+  exit
+}
+
+
+function show_version {
+  cat $UPGRADE_PATH/config/version.yaml
+  exit
+}
+
+
+function upgrade {
+  (flock -n 9 || error "Upgrade is already running. Lock file: ${LOCK_FILE}"
+      run_upgrade "$@"
+  ) 9> $LOCK_FILE
+}
+
+
+
+case "$1" in
+  --switch-to-version)
+   case "$2" in
+     "") error '--switch-to-version requires parameter' ;;
+     *) switch_to_version $2 ; exit ;;
+   esac ;;
+  --version) show_version ; exit ;;
+  *) upgrade "$@" ; exit ;;
+esac
