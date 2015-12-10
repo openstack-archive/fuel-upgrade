bf0bce758eae99f33e4a1484c08c6fadcb7c2ea0
Oleg Gelbukh <ogelbukh@mirantis.com>
2015-12-10 10:21:00 +0000

    Switch to setup.cfg for package configuration
    Add setup.cfg with appropriate configuration settings.
    Add README.rst file with short description of the repo.
    Change authorship to OpenStack.
    Remove obsolete code from setup.py.
    
    Change-Id: I18347a8564c195bfb80aac772f4f7e18ce510e06

--- setup.py
+++ setup.py
@@ -1,60 +1,29 @@
-#    Copyright 2014 Mirantis, Inc.
+# Copyright (c) 2013 Hewlett-Packard Development Company, L.P.
 #
-#    Licensed under the Apache License, Version 2.0 (the "License"); you may
-#    not use this file except in compliance with the License. You may obtain
-#    a copy of the License at
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
 #
-#         http://www.apache.org/licenses/LICENSE-2.0
+#    http://www.apache.org/licenses/LICENSE-2.0
 #
-#    Unless required by applicable law or agreed to in writing, software
-#    distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
-#    WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
-#    License for the specific language governing permissions and limitations
-#    under the License.
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or
+# implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
 
-import os
+# THIS FILE IS MANAGED BY THE GLOBAL REQUIREMENTS REPO - DO NOT EDIT
+import setuptools
 
-from setuptools import find_packages
-from setuptools import setup
+# In python < 2.7.4, a lazy loading of package `pbr` will break
+# setuptools if some other modules registered functions in `atexit`.
+# solution from: http://bugs.python.org/issue15881#msg170215
+try:
+    import multiprocessing  # noqa
+except ImportError:
+    pass
 
-
-def parse_requirements_txt():
-    """Parses requirements.txt
-
-    Returns arrays with `install_requires`
-    packages and with `dependency_links` sources.
-    """
-    root = os.path.dirname(os.path.abspath(__file__))
-
-    requirements = []
-
-    with open(os.path.join(root, 'requirements.txt'), 'r') as f:
-        for line in f.readlines():
-            line = line.rstrip()
-            if not line or line.startswith('#'):
-                continue
-            requirements.append(line)
-
-    return requirements
-REQUIREMENTS = parse_requirements_txt()
-
-
-setup(
-    name='fuel_upgrade',
-    version='0.1.0',
-    description='Upgrade system for Fuel-master node',
-    long_description="""Upgrade system for Fuel-master node""",
-    classifiers=[
-        "Programming Language :: Python",
-        "Topic :: System :: Software Distribution"],
-    author='Mirantis Inc.',
-    author_email='product@mirantis.com',
-    url='http://mirantis.com',
-    keywords='fuel upgrade mirantis',
-    packages=find_packages(),
-    zip_safe=False,
-    install_requires=REQUIREMENTS,
-    include_package_data=True,
-    entry_points={
-        'console_scripts': [
-            'fuel-upgrade = fuel_upgrade.cli:main']})
+setuptools.setup(
+    setup_requires=['pbr>=1.8'],
+    pbr=True)
