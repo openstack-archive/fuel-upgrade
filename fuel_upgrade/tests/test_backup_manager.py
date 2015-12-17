aa83ba37812e192a598a33cd46a88db2aee48c3a
Oleg Gelbukh <ogelbukh@mirantis.com>
2015-12-17 19:17:36 +0000

    Upgrade engine for backup
    Use shotgun to backup configuration files, host keys and
    dump of nailgun and keystone database.
    
    Implements: bp/upgrade-master-node-centos7
    Change-Id: I35b1fc7a0d60951b2399b35361d53664a54f3c21

--- fuel_upgrade/tests/test_backup_manager.py
+++ fuel_upgrade/tests/test_backup_manager.py
@@ -26,6 +26,15 @@ class TestBackupManager(BaseTestCase):
     def setUp(self):
         self.test_config = mock.Mock()
 
+        self.supervisor_patcher = mock.patch(
+            'fuel_upgrade.engines.backup.SupervisorClient')
+        self.supervisor_class = self.supervisor_patcher.start()
+        self.mock_supervisor = mock.MagicMock()
+        self.supervisor_class.return_value = self.mock_supervisor
+
+    def tearDown(self):
+        self.supervisor_patcher.stop()
+
     def _build_config(self):
         self.test_config = config.build_config(None, None)
 
@@ -53,3 +62,20 @@ class TestBackupManager(BaseTestCase):
             '/etc/fuel/version.yaml')
         mock_read_yaml_config.assert_called_once_with(
             '/etc/fuel/astute.yaml')
+
+    @mock.patch("shotgun.config.Config")
+    @mock.patch("shotgun.manager.Manager")
+    def test_upgrade(self, mock_shotgun_manager_class,
+                     mock_shotgun_config_class):
+        mock_shotgun_config = mock_shotgun_config_class.return_value
+        mock_shotgun_manager = mock_shotgun_manager_class.return_value
+
+        upgrader = BackupManager(self.test_config)
+        upgrader.upgrade()
+
+        self.mock_supervisor.start.assume_called_once_with('nailgun')
+        mock_shotgun_config_class.assume_called_once_with(
+            self.test_config.backup_config)
+        mock_shotgun_manager_class.assume_called_once_with(
+            mock_shotgun_config)
+        mock_shotgun_manager.snapshot.assume_called_once()
