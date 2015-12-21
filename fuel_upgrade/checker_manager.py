83bf6b46ad6431639d1adebbe7f1920faec5cf54
Oleg Gelbukh <ogelbukh@mirantis.com>
2015-12-21 14:22:41 +0000

    Backup command for fuel-upgrade script
    Add 'fuel-upgrade backup' command without arguments.
    Add pre-upgrade hooks for the backup command.
    Add checkers for backup command.
    
    Change-Id: Ifbdb2fc2b160d05bb2a2d95949bd94231612852e
    Implements: bp/upgrade-master-node-centos7

--- fuel_upgrade/checker_manager.py
+++ fuel_upgrade/checker_manager.py
@@ -18,6 +18,7 @@ import logging
 
 import six
 
+from fuel_upgrade.engines.backup import BackupManager
 from fuel_upgrade.engines.docker_engine import DockerInitializer
 from fuel_upgrade.engines.docker_engine import DockerUpgrader
 from fuel_upgrade.engines.host_system import HostSystemUpgrader
@@ -49,6 +50,10 @@ class CheckerManager(object):
 
     #: Mapping of checkers to upgrade engines
     CHECKERS_MAPPING = {
+        BackupManager: [
+            CheckRequiredVersion,
+            CheckNoRunningTasks,
+            CheckNoRunningOstf],
         DockerUpgrader: [
             CheckUpgradeVersions,
             CheckRequiredVersion,
